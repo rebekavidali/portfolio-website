dde7c21576e3f0753af944ec1c3ced1234859f95
rebekavidali <103493718+rebekavidali@users.noreply.github.com>
2022-05-05 23:30:24 +0200

    added drop down menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log ("Hi! Welcome to my portfolio site. Enjoy exploring!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+ x.className += ' responsive';
+} else {
+ x.className = 'navtoggle';
+}
+}
